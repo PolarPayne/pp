6f18cc908934c9d20bc361e9c1d9d28f68fe7c6a
Maximilian Remming <maxremming@gmail.com>
2020-02-04 10:13:33 +0200

    Properly get all objects when listing from S3

--- backend.go
+++ backend.go
@@ -31,10 +31,43 @@ func (b *Backend) ListPodcasts() ([]Podcast, error) {
 		return nil, err
 	}
 
-	out := make([]Podcast, 0, len(r.Contents))
-	for _, obj := range r.Contents {
+	contents := r.Contents
+
+	// the S3 API is kinda annoying and we have to jump through some hoops to
+	// make sure we get all of the objects, the go API for this is also not particularly great
+
+	var (
+		isTruncated bool
+		nextMarker  string
+	)
+
+	for {
+		if r.IsTruncated != nil {
+			isTruncated = *r.IsTruncated
+		}
+		if r.NextMarker != nil {
+			nextMarker = *r.NextMarker
+		} else {
+			isTruncated = false
+		}
+
+		if !isTruncated {
+			break
+		}
+
+		r, err = b.s3.ListObjects(&s3.ListObjectsInput{
+			Marker: &nextMarker,
+		})
+		if err != nil {
+			return nil, err
+		}
+		contents = append(contents, r.Contents...)
+	}
+
+	out := make([]Podcast, 0, len(contents))
+	for _, obj := range contents {
 		if obj.Key == nil {
-			return nil, errors.New("invalid S3 object: Key or Size is nil")
+			return nil, errors.New("invalid S3 object: Key is nil")
 		}
 
 		key := *obj.Key
@@ -45,7 +78,7 @@ func (b *Backend) ListPodcasts() ([]Podcast, error) {
 
 		p, err := newPodcast(b, key, obj.Size)
 		if err != nil {
-			log.Printf("invalid podcast %+v: %v", p, err)
+			log.Printf("invalid podcast: %v", err)
 			continue
 		}
 		out = append(out, p)
